c89cc79a75181e8531754fd832a3f799d30a5aa4
Michael Pilosov <consistentbayes@gmail.com>
2025-12-04 22:31:38 -0700

    clean up gh pages build

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '')
-  
-  return {
-    base: env.BASE_PATH || '/',
-    plugins: [react()],
-    server: {
-      port: 5173,
-    },
-  }
+export default defineConfig({
+  base: process.env.BASE_PATH || '/',
+  plugins: [react()],
+  server: {
+    port: 5173,
+  },
 })
